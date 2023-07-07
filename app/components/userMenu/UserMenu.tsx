f82a4348accdb8c62f247a5c1edeb91a9d24dbf0
clearfixx <akulagin88@gmail.com>
2023-07-07 14:45:59 +0300

    google and gihub authorization

--- app/components/userMenu/UserMenu.tsx
+++ app/components/userMenu/UserMenu.tsx
@@ -6,6 +6,7 @@ import styles from "@/app/styles/components/UserMenu.module.scss";
 // Libs
 import { useCallback, useState } from "react";
 import { signOut } from "next-auth/react";
+import { toast } from "react-toastify";
 
 // Hooks
 import useRegisterModal from "@/app/hooks/useRegisterModal";
@@ -19,12 +20,13 @@ import { AiOutlineMenu } from "react-icons/ai";
 import RegionButton from "../regionButton/RegionButton";
 import ProfileIcon from "../profileIcon/ProfileIcon";
 import MenuItem from "../menuItem/MenuItem";
-import { toast } from "react-toastify";
+import Image from "next/image";
 
 const UserMenu: React.FC<INavbraProps> = ({ currentUser }) => {
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const [isOpen, setIsOpen] = useState(false);
+  const userName = currentUser?.name;
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
@@ -38,7 +40,25 @@ const UserMenu: React.FC<INavbraProps> = ({ currentUser }) => {
         <div className={styles.user_menu__profile_button} onClick={toggleOpen}>
           <AiOutlineMenu />
           <div className={styles.profile_icon}>
-            <ProfileIcon width={30} height={30} fill="#717171" />
+            {currentUser ? (
+              <>
+                {currentUser.image ? (
+                  <Image
+                    src={`${currentUser.image}`}
+                    alt={`${currentUser.name}'s avatar`}
+                    width={30}
+                    height={30}
+                    className="rounded-full"
+                  />
+                ) : (
+                  <div className="w-[30px] h-[30px] rounded-full bg-slate-600 text-white flex justify-center items-center font-normal text-xs">
+                    {userName?.charAt(0).toUpperCase()}
+                  </div>
+                )}
+              </>
+            ) : (
+              <ProfileIcon width={30} height={30} fill="#717171" />
+            )}
           </div>
         </div>
       </div>
